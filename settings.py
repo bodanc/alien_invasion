329f6d3117a43a54e0deb6549e916f396c9176d5
Bogdan <bogdan@reynencourt.com>
2022-01-21 00:11:59 +0100

    implements collisions between bullets and aliens

--- settings.py
+++ settings.py
@@ -7,20 +7,20 @@ class Settings:
         # Screen settings
         self.screen_width = 1200
         self.screen_height = 800
-        self.bg_color = (220, 220, 220)
+        self.bg_color = (230, 230, 230)
 
         # Ship settings
         self.ship_speed = 0.5
 
         # Bullet settings
-        self.bullet_speed = 1.0
-        self.bullet_width = 3
+        self.bullet_speed = 1.5
+        self.bullet_width = 2
         self.bullet_height = 15
         self.bullet_color = (60, 60, 60)
-        self.bullets_allowed = 3
+        self.bullets_allowed = 4
 
         # Alien settings
         self.alien_speed = 0.3
         self.fleet_drop_speed = 10
-        # A fleet direction of 1 represents right and -1 represents left.
+        # A fleet direction of 1 represents movement to the right and -1 to the left.
         self.fleet_direction = 1
